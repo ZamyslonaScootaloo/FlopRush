34a51a1dd51fb39a25d88b6296e5cfc5697e4b08
Spaziopl <62474122+Spaziopl@users.noreply.github.com>
2020-09-23 18:24:44 +0200

    Dodanie Zycie Przeciwnika i Proste poruszanie się do Punku A

---
+++ FlopRush/Assets/Scripts/Enemy/EnemyStats.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyStats : MonoBehaviour
+{
+	[Header("Health")]
+    public float health; //życie przeciwnika
+    public float maxHealth; //ile przeciwnik moze miec max hp ( można edytować w unity )
+
+	public float dodajzycie; //ile przeciwnik traci czy dostaje zycie. jeżeli chcemy aby tracił ustawiamy wartość na minus ( - )
+
+
+	public void Start()
+	{
+		health = maxHealth; // Ustawiamy zycie na samym początku na maxa
+	}
+	public void Update()
+	{
+		#region Health
+		if (health >= maxHealth) // jeżeli zycie jest wieksze to ustaw na początkową wartośc
+		{
+			health = maxHealth;
+		} else if(health <= 0) // jeżeli życie jest mniejsze to ustaw na 0
+		{
+			health = 0;
+		}
+		#endregion 
+		if (Input.GetKeyDown(KeyCode.H)) // Sprawdzamy czy przeciwnik traci albo dostaje zycie
+		{
+			HealthModify(dodajzycie);
+		}
+	}
+
+
+	public void HealthModify(float value) // Funkcja sprawdzająca czy przeciwnik traci albo dostaje zycie
+	{
+		health += value;
+	}
+}
---
+++ FlopRush/Assets/Scripts/Enemy/NavMeshmovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavMeshmovement : MonoBehaviour
+{
+    [SerializeField]
+    Transform pointA; //Obiekt do którego ma iść przeciwnik
+    NavMeshAgent _navMeshAgent; //NavMeshAgent musi być w przeciwniku 
+    void Start()
+    {
+        _navMeshAgent = this.gameObject.transform.GetComponent<NavMeshAgent>(); // pobierz _nMA z tego obieku
+
+
+    }
+	public void Update()
+	{
+		if(_navMeshAgent != null) //jeżeliw obiekcie jest _navMeshAgent to idz w strone pointA
+		{
+            SetDistination();
+
+        }
+		else
+		{
+            Debug.Log("Nie ma NavMeshu w gameObjecie");
+		}
+    }
+
+	public void SetDistination() // Bierzemy pozycje z PointA i ustawiamy wartość dla NavMeshu
+	{
+        Vector3 targetPoint = pointA.transform.position;
+        _navMeshAgent.SetDestination(targetPoint);
+    }
+}
